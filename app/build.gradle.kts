4ed35c4088cb2e6cf3321aee9a97487eae9c6f4b
oz gutman <ozgutman485@gmail.com>
2025-01-27 16:19:58 +0200

    first commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,14 +32,7 @@ android {
 }
 
 dependencies {
-
-    implementation(libs.appcompat)
-    implementation(libs.material)
-    implementation(libs.activity)
-    implementation(libs.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.ext.junit)
-    androidTestImplementation(libs.espresso.core)
+    // Core Android dependencies
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
